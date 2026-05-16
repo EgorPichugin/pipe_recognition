7ef993e1c873f93c054a73169ec7cbe8fe23b735
Egor Pichugin <pichugin.egor1993@gmail.com>
2026-05-16 18:05:32 +0200

    * changed gitignore

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 
 from dotenv import load_dotenv
 from fastapi import FastAPI, File, Form, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
 
 load_dotenv()
@@ -26,6 +27,17 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="Pipe Recognition API", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
